54753ce6d484a76a0185949f2e5e6729498100ee
lvh <_@lvh.io>
2017-09-11 14:08:54 -0500

    Add core functionality

--- src/unsiemly/core.clj
+++ src/unsiemly/core.clj
@@ -1,24 +1,18 @@
 (ns unsiemly.core
   (:gen-class)
-  (:require [clojure.set :as set]
-            [clojure.spec.alpha :as s]
-            [unsiemly.stackdriver :as stackdriver]
-            [unsiemly.elasticsearch :as elasticsearch]
-            [environ.core :refer [env]]
-            [manifold.stream :as ms]))
-
-(s/def ::siem-type #{:stdout :elasticsearch :aws-elasticsearch :stackdriver})
-
-(defmulti opts-spec ::siem-type)
-(s/def ::opts (s/multi-spec opts-spec ::siem-type))
-
-(defmethod opts-spec ::stdout [_] (s/keys :req [::siem-type]))
+  (:require
+   [unsiemly.internal :as internal]
+   [unsiemly.stackdriver :as stackdriver]
+   [unsiemly.elasticsearch :as elasticsearch]
+   [manifold.stream :as ms]))
 
 (defn ->siem!
   "Consume everything in the given source, transform it and send it to a SIEM, as
   described by the given opts."
   [source opts]
-  )
+  (->> source
+       (ms/batch 1 0)
+       (ms/consume (internal/entries-callback opts))))
 
 (defn siem-sink!
   "Build a stream (sink) that consumes general events, cleans them up, and
@@ -26,4 +20,6 @@
 
   If you already have a stream, look at [[->siem!]]."
   [opts]
-  (->siem! (ms/stream) opts))
+  (let [s (ms/stream)]
+    (->siem! s opts)
+    s))
--- test/unsiemly/core_test.clj
+++ test/unsiemly/core_test.clj
@@ -1,1 +1,13 @@
-(ns unsiemly.core-test)
+(ns unsiemly.core-test
+  (:require [clojure.test :as t]
+            [unsiemly.core :as c]
+            [manifold.stream :as ms]))
+
+(alias 'u 'unsiemly)
+
+(t/deftest stdout-siem-sink!-test
+  (let [s (c/siem-sink! {::u/siem-type :stdout
+                         ::u/log-name "test log"})]
+    (t/is (= "{:my event}\n{:my second event}\n"
+             (with-out-str
+               @(ms/put-all! s [{:my "event"} {:my "second event"}]))))))

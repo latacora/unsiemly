c3c9615514116f512424dc6263446c3257299f59
lvh <_@lvh.io>
2017-12-01 15:04:05 -1000

    Add JSON stdout format

--- src/unsiemly/stdout.clj
+++ src/unsiemly/stdout.clj
@@ -1,21 +1,28 @@
 (ns unsiemly.stdout
   (:require [unsiemly.internal :as internal]
             [clojure.spec.alpha :as s]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [cheshire.core :as json]))
 
 (alias 'u 'unsiemly)
 
+(s/def ::format #{:str :json})
 (s/def ::pretty-printed boolean?)
 (s/def ::opts
   (s/and
    ::u/base-opts
-   (s/keys :opt [::pretty-printed])))
+   (s/keys :opt [::pretty-printed ::format])))
 
 (defmethod internal/opts-spec :stdout [_]
   ::opts)
 
 (defmethod internal/entries-callback :stdout
-  [{::keys [pretty-printed]}]
-  (let [printer (if pretty-printed pp/pprint println)]
+  [{::keys [pretty-printed format] :or {format :str}}]
+  (let [printer
+        (case format
+          :str (if pretty-printed pp/pprint println)
+          :json (fn [obj]
+                  (json/generate-stream obj *out* {:pretty pretty-printed})
+                  (println)))]
     (fn [entries]
       (doseq [e entries] (printer e)))))
--- test/unsiemly/stdout_test.clj
+++ test/unsiemly/stdout_test.clj
@@ -7,7 +7,7 @@
 
 (alias 'u 'unsiemly)
 
-(t/deftest stdout-siem-sink!-test
+(t/deftest stdout-str-siem-sink!-test
   (let [s (c/siem-sink! {::u/siem-type :stdout
                          ::u/log-name "test log"})]
     (t/is (= "{:my event}\n{:my second event}\n"
@@ -20,6 +20,28 @@
         regular (with-out-str @(ms/put-all! s deeply-nested))
         s (c/siem-sink! {::u/siem-type :stdout
                          ::u/log-name "test log"
+                         ::stdout/pretty-printed true})
+        pretty-printed (with-out-str @(ms/put-all! s deeply-nested))
+        whitespace-chars #(count (filter (comp str/blank? str) %))]
+    (t/is (< (whitespace-chars regular) (whitespace-chars pretty-printed))
+          "pretty printed string has more whitespace than regular printed string")))
+
+(t/deftest stdout-json-siem-sink!-test
+  (let [s (c/siem-sink! {::u/siem-type :stdout
+                         ::u/log-name "test log"
+                         ::stdout/format :json})]
+    (t/is (= "{\"my\":\"event\"}\n{\"my\":\"second event\"}\n"
+             (with-out-str
+               @(ms/put-all! s [{:my "event"} {:my "second event"}])))))
+
+  (let [s (c/siem-sink! {::u/siem-type :stdout
+                         ::u/log-name "test log"
+                         ::stdout/format :json})
+        deeply-nested [(update-in {} (range 20) :xyzzy)]
+        regular (with-out-str @(ms/put-all! s deeply-nested))
+        s (c/siem-sink! {::u/siem-type :stdout
+                         ::u/log-name "test log"
+                         ::stdout/format :json
                          ::stdout/pretty-printed true})
         pretty-printed (with-out-str @(ms/put-all! s deeply-nested))
         whitespace-chars #(count (filter (comp str/blank? str) %))]

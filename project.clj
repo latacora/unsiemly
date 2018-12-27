67606abba25ab11e24e2efff7ca078b835213482
Laurens Van Houtven (lvh) <_@lvh.io>
2018-12-27 15:21:44 -0600

    Upgrade deps

--- project.clj
+++ project.clj
@@ -4,29 +4,29 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [manifold "0.1.7-alpha5"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [manifold "0.1.8"]
                  [environ "1.1.0"]
                  [clojure.java-time "0.3.2"]
                  [camel-snake-kebab "0.4.0"]
                  [com.taoensso/timbre "4.10.0"]
 
                  ;; xforms
-                 [com.rpl/specter "1.1.0"]
+                 [com.rpl/specter "1.1.2"]
 
                  ;; -> ElasticSearch
-                 [cc.qbits/spandex "0.6.2"]
+                 [cc.qbits/spandex "0.6.4"]
 
                  ;; -> AWS ES
-                 [vc.inreach.aws/aws-signing-request-interceptor "0.0.20"]
-                 [com.amazonaws/aws-java-sdk-core "1.11.312"
+                 [vc.inreach.aws/aws-signing-request-interceptor "0.0.22"]
+                 [com.amazonaws/aws-java-sdk-core "1.11.475"
                   :exclusions [commons-logging]]
 
                  ;; -> StackDriver
-                 [com.google.cloud/google-cloud-logging "1.25.0"]
+                 [com.google.cloud/google-cloud-logging "1.56.0"]
 
                  ;; -> BigQuery
-                 [com.google.cloud/google-cloud-bigquery "1.25.0"]]
+                 [com.google.cloud/google-cloud-bigquery "1.56.0"]]
 
   :main ^:skip-aot unsiemly.core
   :target-path "target/%s"
@@ -39,7 +39,7 @@
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]
                                   ;; ;; just for joda-time conversions:
-                                  [clj-time "0.14.3"]]}}
+                                  [clj-time "0.15.1"]]}}
 
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]])
